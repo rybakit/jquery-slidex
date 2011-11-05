3dc85c12379362958dd0d6f2d2c98981676318f2
Eugene Leonovich <gen.work@gmail.com>
2011-11-06 00:46:06 +0100

    simplified code

--- src/js/jquery.slideshow.js
+++ src/js/jquery.slideshow.js
@@ -1,68 +1,73 @@
 (function($) {
 
-  var isShowing = false, timer;
+    var defaults = {
+        interval: 5,
+        speed: 'slow',
+    };
 
-  $.slideshow = function(target, config) {
-    this.config = $.extend(true, {}, $.slideshow.defaults, config);
-    this.target = target;
-    this.init();
-  };
+    $.slideshow = function(target, config) {
+        var self = this, isShowing = false, timer;
 
-  $.extend($.slideshow, {
-    defaults: {
-      interval: 5,
-      speed: 'slow',
-    },
+        self.config = $.extend(defaults, config);
+        self.target = target;
+        self.slides = $(self.target).children();
+        self.currentIndex = 0;
 
-    prototype: {
-      init: function() {
-        this.slides = $(this.target).children();
-        this.currentIndex = 0;
-        this.current = $(this.slides[this.currentIndex]).css('z-index', 2);
-      },
+        $(self.slides[self.currentIndex]).css('z-index', 2);
 
-      show: function() {
-        if (isShowing) {
-          return;
-        }
-        //console.log(this.target.id + ': ' + new Date());
-        isShowing = true;
+        self.show = function() {
+            if (isShowing) {
+                return false;
+            }
+            isShowing = true;
 
-        var self = this, 
-          nextIndex = self.currentIndex == self.slides.length - 1 ? 0 : self.currentIndex + 1;
-          next = $(self.slides[nextIndex]);
+            var current = $(self.slides[self.currentIndex]),
+                nextIndex = self.currentIndex == self.slides.length - 1 ? 0 : self.currentIndex + 1;
+                next = $(self.slides[nextIndex]);
 
-        self.change(self.current, next, function() {
-          self.current = next;
-          self.currentIndex = nextIndex;
-          isShowing = false;
+            self.change(current, next, function() {
+                self.currentIndex = nextIndex;
+                isShowing = false;
+            });
+        };
+
+        self.change = function(from, to, callback) {
+            /*
+            if (2 != from.css('z-index')) {
+                console.log('z-index: ' + from.css('z-index') + ', currIndex: ' + self.currentIndex);
+            }
+            */
+
+            to.css('z-index', 1);
+            from.fadeOut(self.config.speed, function() {
+                to.css('z-index', 2);
+                from.css('z-index', 0).show();
+                callback.call(self);
+            });
+        };
+
+        self.start = function() {
+            /*
+            self.show();
+            setTimeout(function() { self.show(); }, 1000);
+            setTimeout(function() { self.show(); }, 2000);
+            setTimeout(function() { self.show(); }, 3000);
+            */
+
+            timer = setInterval(function() { self.show(); }, self.config.interval * 1000);
+        };
+
+
+        self.stop = function() {
+            clearInterval(timer);
+        };
+    };
+
+    $.fn.slideshow = function(config) {
+        return this.each(function() {
+            var slideshow = new $.slideshow(this, config);
+            slideshow.start();
         });
-      },
-
-      change: function(from, to, callback) {
-        to.css('z-index', 1);
-        from.fadeOut(this.config.speed, function() {
-          to.css('z-index', 2);
-          from.css('z-index', 0).show();
-          callback();
-        });
-      },
-
-      start: function() {
-        timer = setInterval($.proxy(this.show, this), this.config.interval * 1000);
-      },
-
-      stop: function() {
-        clearInterval(timer);
-      }
     }
-  });
-
-  $.fn.slideshow = function(config) {
-    return this.each(function() {
-      var slideshow = new $.slideshow(this, config);
-      slideshow.start();
-    });
-  }
 
 }(jQuery));
