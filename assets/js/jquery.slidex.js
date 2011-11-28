0592775b1925ef0f57014ed04553ccd84a60a18a
Eugene Leonovich <gen.work@gmail.com>
2011-11-28 23:47:54 +0100

    added ability to decorate slader instance, renamed $.slideshow to $.slidex

---
+++ assets/js/jquery.slidex.js
@@ -0,0 +1,73 @@
+(function($) {
+    $.slidex = function(target, config) {
+        this.config = $.extend($.slidex.defaults, config);
+        this.target = target;
+        this.init();
+    };
+
+    $.extend($.slidex, {
+        defaults: {
+            interval: 5,
+            speed: 'slow',
+            transition: function(from, to) {
+                from.addClass('last-active').removeClass('active');
+                return to.hide().addClass('active').fadeIn(this.config.speed, function() {
+                    from.removeClass('last-active');
+                });
+            }
+        },
+
+        prototype: {
+            constructor: $.slidex,
+
+            init: function() {
+                this.slides = $(this.target).children();
+                this.index = $('>.active', this.target).index();
+                if (-1 == this.index) {
+                    $(this.slides[ this.index = 0 ]).addClass('active');
+                }
+            },
+
+            show: function(index) {
+                var self = this;
+
+                if (self.locked) {
+                    return false;
+                }
+                self.locked = true;
+
+                if ('undefined' === typeof index) {
+                    index = self.index == self.slides.length - 1 ? 0 : self.index + 1;
+                }
+
+                $(self).trigger('before.slidex', [ self.index, index ]);
+                $.when(
+                    self.config.transition.call(self, $(self.slides[self.index]), $(self.slides[index]))
+                ).done(function() {
+                    self.index = index;
+                    $(self).trigger('after.slidex');
+                    self.locked = false;
+                });
+            },
+
+            play: function() {
+                var self = this;
+                if (self.timer) {
+                    clearInterval(self.timer);
+                }
+                self.timer = setInterval(function() { self.show(); }, self.config.interval * 1000);
+            }
+        }
+    });
+
+    $.fn.slidex = function(config, decorate) {
+        return this.each(function() {
+            var slidex = new $.slidex(this, config);
+            if ($.isFunction(decorate)) {
+                decorate(slidex);
+            }
+            slidex.play();
+        });
+    }
+
+}(jQuery));
---
+++ assets/js/jquery.slidex.pagination.js
@@ -0,0 +1,28 @@
+(function($) {
+    ($.slidex.ext = $.slidex.ext || {}).pagination = function(slidex, config) {
+        // TODO refactor
+        var container = $(config.container);
+        var pages = container.children();
+        var plen = pages.length, slen = slidex.slides.length;
+
+        if (!plen || !slen) {
+            $.error('No pages/slides were found.');
+        }
+
+        while (plen < slen) {
+            pages.last().clone().appendTo(container);
+            plen++;
+        }
+        pages = container.children();
+
+        pages.click(function() {
+            slidex.show($(this).index());
+        });
+        $(slidex).bind({
+            'before.slidex': function(e, currentIndex, nextIndex) {
+                //pages[currentIndex].animate({ opacity: 0.4 }, slidex.config.speed);
+                //pages[nextIndex].animate({ opacity: 0.8 }, slidex.config.speed);
+            }
+        });
+    }
+}(jQuery));
