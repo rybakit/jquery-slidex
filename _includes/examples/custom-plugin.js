f2540ced16fc1d9cad0dd7bf6642fb7654d38e2e
Eugene Leonovich <gen.work@gmail.com>
2011-12-17 20:56:47 +0100

    applied minor fixes

--- _includes/examples/custom-plugin.js
+++ _includes/examples/custom-plugin.js
@@ -1,11 +1,11 @@
 $('.image-slides').slidex(null, function(slidex) {
-    var bar = $('.progress-bar p');
+    var bar = $('.slides .progress-bar p');
 
     function anim() {
         bar.animate({ width: '100%' }, slidex.config.delay * 1000, function() {
             $(this).width(0);
         });
-    };
+    }
 
     anim();
     $(slidex).bind('before.slidex', anim);
