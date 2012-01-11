17749d3190751d86e6d7999a1bd14bdc2fd5d05c
Eugene Leonovich <gen.work@gmail.com>
2012-01-11 22:35:30 +0100

    added ability to guess the position of the caption

--- assets/js/jquery.slidex.caption.js
+++ assets/js/jquery.slidex.caption.js
@@ -1,15 +1,29 @@
 (function($) {
     ($.slidex.ext = $.slidex.ext || {}).caption = function(slidex, container) {
         container = $(container);
-        var items = container.children();
+
+        var items = container.children(), target = $(slidex.target),
+            pos = 'bottom', dim = 'height';
+
+        if (container.css('top') == target.css('top')) {
+            if (container.width() >= target.height()) {
+                pos = 'top';
+            } else {
+                pos = container.css('left') == target.css('left') ? 'left' : 'right';
+                dim = 'width';
+            }
+        }
 
         $(items[slidex.index]).show();
 
         $(slidex).bind('before.slidex', function(e, oldIndex, newIndex) {
-            container.animate({ bottom: '-' + container.css('height'), opacity: 0 }, 'slow', function() {
+            var props = { opacity: 'toggle' };
+            props[pos] = '-' + container.css(dim);
+            container.animate(props, 'slow', function() {
                 $(items[oldIndex]).hide();
                 $(items[newIndex]).show();
-                container.animate({ bottom: 0, opacity: 1 }, 'slow');
+                props[pos] = 0;
+                container.animate(props, 'slow');
             });
         });
     };
