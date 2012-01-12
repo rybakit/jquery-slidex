839c1e3991e7c111cf533f5c02a37699934a5d75
Eugene Leonovich <gen.work@gmail.com>
2012-01-12 21:19:19 +0100

    fixed CS

--- _includes/examples/custom-transition/example.js
+++ _includes/examples/custom-transition/example.js
@@ -1,12 +1,12 @@
 $('.slides').slidex({ transition: function(from, to) {
     from.addClass('slidex-semi-active').removeClass('slidex-active');
     return to.css('top', '-' + to.css('height'))
-      .addClass('slidex-active')
-      .animate({ top: 0 }, {
-          duration: this.config.speed,
-          specialEasing: { top: 'easeOutBounce' },
-          complete: function() {
-              from.removeClass('slidex-semi-active');
-          }
-      });
+        .addClass('slidex-active')
+        .animate({ top: 0 }, {
+            duration: this.config.speed,
+            specialEasing: { top: 'easeOutBounce' },
+            complete: function() {
+                from.removeClass('slidex-semi-active');
+            }
+        });
 }});
