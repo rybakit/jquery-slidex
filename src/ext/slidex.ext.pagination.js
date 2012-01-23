4d7def40c7364254784a0670b1e2b0892fd64745
Eugene Leonovich <gen.work@gmail.com>
2012-01-23 21:31:36 +0100

    updated sources, removed outdated files

---
+++ src/ext/slidex.ext.pagination.js
@@ -0,0 +1,21 @@
+(function($) {
+    "use strict";
+
+    ($.slidex.ext = $.slidex.ext || {}).pagination = function(slidex, container) {
+        var pages = $(container).children().click(function(e) {
+            e.preventDefault();
+            slidex.show($(this).index());
+        });
+
+        for (var i = pages.length, len = slidex.slides.length; i < len; i++) {
+            pages[i] = pages.last().clone(true).appendTo(container);
+        }
+
+        $(pages[slidex.index]).css('opacity', .8);
+
+        $(slidex).bind('before.slidex', function(e, oldIndex, newIndex) {
+            $(pages[oldIndex]).animate({ opacity: .4 }, slidex.config.speed);
+            $(pages[newIndex]).animate({ opacity: .8 }, slidex.config.speed);
+        });
+    };
+}(window.jQuery));
