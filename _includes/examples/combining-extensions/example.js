82efe082966a8abdb6644bfbeedc2032326ec448
Eugene Leonovich <gen.work@gmail.com>
2012-01-24 22:52:32 +0100

    removed local versions of js files, updated templates

--- _includes/examples/combining-extensions/example.js
+++ _includes/examples/combining-extensions/example.js
@@ -1,4 +1,4 @@
 $('.image-slides').slidex(null, function(slidex) {
-    $.slidex.ext.pagination(slidex, '.slidex-pagination');
     $.slidex.ext.caption(slidex, '.slidex-caption');
+    $.slidex.ext.pagination(slidex, '.slidex-pagination');
 });
