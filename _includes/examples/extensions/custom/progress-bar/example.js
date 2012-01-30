85297a620bfd78dbd61ed1dc705941f899779697
Eugene Leonovich <gen.work@gmail.com>
2012-01-30 21:25:27 +0100

    enhanced progress bar js example

--- _includes/examples/extensions/custom/progress-bar/example.js
+++ _includes/examples/extensions/custom/progress-bar/example.js
@@ -1,12 +1,13 @@
 $('.image-slides').slidex(null, function(slidex) {
     var bar = $('.slides .progress-bar p');
 
-    function anim() {
-        bar.animate({ width: '100%' }, slidex.config.delay * 1000, function() {
-            $(this).width(0);
+    $(slidex)
+        .bind('start.slidex before.slidex', function () {
+            bar.animate({ width: '100%' }, slidex.config.delay * 1000, function() {
+                    bar.width(0);
+                });
+            })
+        .bind('stop.slidex', function() {
+            bar.stop(true, true);
         });
-    }
-
-    anim();
-    $(slidex).bind('before.slidex', anim);
 });
